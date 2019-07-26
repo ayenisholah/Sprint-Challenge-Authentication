b80106624d8894c242e831964fb6da50b352de0a
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-26 10:33:17 +0100

    add users test

---
+++ config/routes.spec.js
@@ -0,0 +1,46 @@
+const db = require('../database/dbConfig');
+const Users = {
+  add,
+  find,
+  findBy,
+  findById,
+};
+
+function find() {
+  return db('users').select('id', 'username', 'password');
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+async function add(user) {
+  const [id] = await db('users').insert(user);
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
+// Test
+
+beforeEach(async () => {
+  await db('users').truncate();
+});
+
+describe('Users.insert', () => {
+  it('is able to add users', async () => {
+    let users = await Users.find();
+    expect(users).toHaveLength(0)
+
+    await Users.add({ username: 'alex', password: 1234 });
+    await Users.add({ username: 'matt', password: 1234 });
+    users = await Users.find();
+
+    expect(users).toHaveLength(2)
+  })
+})
+
