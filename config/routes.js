4923e593058ecb9f2e907cde10d81d27f9beb15d
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-26 08:51:11 +0100

    register endpoint

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+const router = require('express').Router()
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const axios = require('axios');
@@ -13,8 +13,16 @@ module.exports = server => {
 
 function register(req, res) {
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10);
+  const hash = bcrypt.hashSync(user.password, 12);
   user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
