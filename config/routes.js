972c60864c3ac28d6f396804c69c4b6d83442eac
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-26 08:57:39 +0100

    login endpoint

--- config/routes.js
+++ config/routes.js
@@ -26,7 +26,25 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
